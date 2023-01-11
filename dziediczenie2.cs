92b3484054797c815b9b4e043d9dd8fcde7e23ef
dejvid987 <114577003+dejvid987@users.noreply.github.com>
2023-01-11 10:39:24 +0100

    Create dziediczenie2.cs

---
+++ dziediczenie2.cs
@@ -0,0 +1,99 @@
+/*public class Credit
+{
+    private string nazwa;
+    private string type;
+    private void expDate()
+    {
+        Console.WriteLine("ExpDate");
+    }
+    public void authorized(){
+        Console.WriteLine("Autortzacja");
+    }
+}
+public class Check
+{
+    private string nazwa;
+    private string bankID;
+    public void authorized(){
+        Console.WriteLine("Autortyzacja");
+    }
+}
+public class Cash
+{
+    private void cashTendered()
+    {
+        Console.WriteLine("Kesz");
+    }
+}
+public class Payment
+{
+    private float amount;
+}
+public class Item
+{
+    private void shippingWeight()
+    {
+        Console.WriteLine("waga");
+    }
+    private string description;
+    public int getPriceForQuantity()
+    {
+        return 1110;
+    }
+    public int getTax()
+    {
+        return 163274;
+    }
+    public void inStock()
+    {
+        Console.WriteLine("w magazynie");
+    }
+}
+public class OrderDeatil
+{
+    private void quality()
+    {
+        Console.WriteLine("Jakość jest super");
+    }
+    private string taxStatus;
+    public void calcSubTotal()
+    {
+        Console.WriteLine("Kalkulowanie");
+    }
+    public void calcWeight()
+    {
+        Console.WriteLine("Waga");
+    }
+    public void calcTax()
+    {
+        Console.WriteLine("Tax");
+    }
+}
+public class Customer
+{
+    private string name;
+    private void adress()
+    {
+        Console.WriteLine("Adres");
+    }
+}
+public class Order
+{
+    private Date date;
+    private string status;
+    public void calcSubTotal() {
+        Console.WriteLine("Kalkulowanie");
+    }
+    public void calcTax()
+    {
+        Console.WriteLine("tax");
+    }
+    public void calcTotal()
+    {
+        Console.WriteLine("razem...");
+    }
+    public void calcTotalWeight()
+    {
+        Console.WriteLine("Razem waga to...");
+    }
+}*/
